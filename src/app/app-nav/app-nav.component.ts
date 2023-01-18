2420383ab563d5e7843f88b5271613a8e5c8a9d7
Graeme <graemefolk@gmail.com>
2023-01-17 21:19:41 -0700

    dynamic menu items based on logged in state

--- src/app/app-nav/app-nav.component.ts
+++ src/app/app-nav/app-nav.component.ts
@@ -2,20 +2,32 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-app-nav',
   templateUrl: './app-nav.component.html',
-  styleUrls: ['./app-nav.component.scss']
+  styleUrls: ['./app-nav.component.scss'],
 })
 export class AppNavComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
+  isLoggedIn: boolean = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public auth: AuthService
+  ) {
+    this.auth.user$.subscribe((user) => {
+      if (user) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
+  }
 }
