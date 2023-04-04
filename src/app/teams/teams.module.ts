60047d66e037c328b8e3c119d1e074542966adf5
Graeme Folk <graemefolk@gmail.com>
2023-04-04 12:12:46 -0600

    update layout with new settings button

--- src/app/teams/teams.module.ts
+++ src/app/teams/teams.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+import { NthPipe } from './pipes/nth.pipe';
+import { RelativeDatePipe } from './pipes/relative-date.pipe';
+import { TeamComponent } from './team/team.component';
 import { TeamsRoutingModule } from './teams-routing.module';
 import { TeamsComponent } from './teams.component';
-import { TeamComponent } from './team/team.component';
-import { NthPipe } from './pipes/nth.pipe';
-import { FormsModule } from '@angular/forms';
-import { RelativeDatePipe } from './pipes/relative-date.pipe';
 
 @NgModule({
   declarations: [TeamsComponent, TeamComponent, NthPipe, RelativeDatePipe],
@@ -25,6 +26,7 @@ import { RelativeDatePipe } from './pipes/relative-date.pipe';
     MatSlideToggleModule,
     MatDividerModule,
     MatSnackBarModule,
+    MatIconModule,
     SharedModule,
   ],
   providers: [RelativeDatePipe],
