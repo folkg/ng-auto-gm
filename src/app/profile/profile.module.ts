c59c4d47283b90f3f32094868c622334bb3ad422
Graeme Folk <graemefolk@gmail.com>
2023-04-04 14:41:36 -0600

    move the ProfileCardComponent into Shared module

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,26 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { SharedModule } from '../shared/shared.module';
-import { ProfileCardComponent } from './profile-card/profile-card.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 
 @NgModule({
-  declarations: [ProfileComponent, ProfileCardComponent],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule,
-    ReactiveFormsModule,
-    MatCardModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    SharedModule,
-  ],
+  declarations: [ProfileComponent],
+  imports: [CommonModule, ProfileRoutingModule, MatButtonModule, SharedModule],
 })
 export class ProfileModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { ProfileCardComponent } from '../shared/profile-card/profile-card.component';
 import { OfflineWarningCardComponent } from './offline-warning-card/offline-warning-card.component';
 import { RobotsComponent } from './robots/robots.component';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
@@ -13,8 +17,17 @@ import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.componen
     OfflineWarningCardComponent,
     RobotsComponent,
     ConfirmDialogComponent,
+    ProfileCardComponent,
   ],
-  imports: [CommonModule, MatDialogModule, MatCardModule, MatButtonModule],
-  exports: [OfflineWarningCardComponent, RobotsComponent],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
+  exports: [OfflineWarningCardComponent, RobotsComponent, ProfileCardComponent],
 })
 export class SharedModule {}
