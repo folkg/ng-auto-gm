9aee8fa64e4290c52aee6fef02f79a2c48a1dc7e
Graeme Folk <graemefolk@gmail.com>
2023-04-05 12:08:11 -0600

    move ProfileCard back to Profile module for better organization

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,33 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { ProfileCardComponent } from '../shared/profile-card/profile-card.component';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { OfflineWarningCardComponent } from './offline-warning-card/offline-warning-card.component';
 import { RobotsComponent } from './robots/robots.component';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
     OfflineWarningCardComponent,
     RobotsComponent,
     ConfirmDialogComponent,
-    ProfileCardComponent,
   ],
-  imports: [
-    CommonModule,
-    MatDialogModule,
-    MatCardModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-  ],
-  exports: [OfflineWarningCardComponent, RobotsComponent, ProfileCardComponent],
+  imports: [CommonModule, MatDialogModule, MatCardModule, MatButtonModule],
+  exports: [OfflineWarningCardComponent, RobotsComponent],
 })
 export class SharedModule {}
--- src/app/teams/teams.module.ts
+++ src/app/teams/teams.module.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { SharedModule } from '../shared/shared.module';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
-import { FormsModule } from '@angular/forms';
+import { ProfileModule } from '../profile/profile.module';
+import { SharedModule } from '../shared/shared.module';
 import { NthPipe } from './pipes/nth.pipe';
 import { RelativeDatePipe } from './pipes/relative-date.pipe';
 import { TeamComponent } from './team/team.component';
@@ -30,6 +30,7 @@ import { TeamsComponent } from './teams.component';
     MatIconModule,
     MatTooltipModule,
     SharedModule,
+    ProfileModule,
   ],
   exports: [TeamComponent],
   providers: [RelativeDatePipe],
