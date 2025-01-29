adf6e6ef26e88cfe39a48b5b1acd0b75ac37a677
Graeme Folk <graemefolk@gmail.com>
2025-01-28 22:48:29 -0700

    fix: Fix app-nav reactivity

--- src/app/app-nav/app-nav.component.ts
+++ src/app/app-nav/app-nav.component.ts
@@ -1,6 +1,6 @@
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 import { AsyncPipe, NgIf } from "@angular/common";
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, computed } from "@angular/core";
 import { MatIconButton } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
 import { MatListItem, MatNavList } from "@angular/material/list";
@@ -12,9 +12,10 @@ import {
 import { MatToolbar } from "@angular/material/toolbar";
 import { MatTooltip } from "@angular/material/tooltip";
 import { RouterLink, RouterOutlet } from "@angular/router";
-import { Observable, Subscription } from "rxjs";
+import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
+import { toSignal } from "@angular/core/rxjs-interop";
 import { AuthService } from "../services/auth.service";
 import { SyncTeamsService } from "../services/sync-teams.service";
 import { ThemingService } from "../services/theming.service";
@@ -40,15 +41,20 @@ import { shareLatest } from "../shared/utils/shareLatest";
     AsyncPipe,
   ],
 })
-export class AppNavComponent implements OnInit, OnDestroy {
-  isHandset$: Observable<boolean> = this.breakpointObserver
+export class AppNavComponent {
+  readonly isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
       map((result) => result.matches),
       shareLatest(),
     );
-  isLoggedIn: boolean = false;
-  hasTransactionsEnabled: boolean = false;
+  private readonly user = toSignal(this.auth.user$);
+  readonly isLoggedIn = computed(() => (this.user() ? true : false));
+
+  private readonly teams = toSignal(this.sts.teams$, { initialValue: [] });
+  readonly hasTransactionsEnabled = computed(() =>
+    this.teams().some((team) => team.allow_transactions),
+  );
 
   constructor(
     private readonly breakpointObserver: BreakpointObserver,
@@ -56,32 +62,6 @@ export class AppNavComponent implements OnInit, OnDestroy {
     readonly auth: AuthService,
     private readonly sts: SyncTeamsService,
   ) {}
-
-  private readonly subs = new Subscription();
-
-  ngOnInit(): void {
-    this.subs.add(
-      this.auth.user$.subscribe((user) => {
-        if (user) {
-          this.isLoggedIn = true;
-        } else {
-          this.isLoggedIn = false;
-        }
-      }),
-    );
-
-    this.subs.add(
-      this.sts.teams$.subscribe((teams) => {
-        this.hasTransactionsEnabled = teams.some(
-          (team) => team.allow_transactions,
-        );
-      }),
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subs.unsubscribe();
-  }
 
   toggleDarkMode() {
     this.themingService.darkModeOn = !this.themingService.darkModeOn;
