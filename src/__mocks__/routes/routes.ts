7905cb532dd08e62fed976763343754d27f362c2
Graeme Folk <graemefolk@gmail.com>
2025-02-07 23:02:31 -0700

    chore: Fix test error for duplicate selectors

--- src/__mocks__/routes/routes.ts
+++ src/__mocks__/routes/routes.ts
@@ -1,21 +1,45 @@
 import { Component } from "@angular/core";
 
-@Component({ standalone: true, template: "Mock Login Page" })
+@Component({
+  standalone: true,
+  template: "Mock Login Page",
+  selector: "app-mock-login",
+})
 class MockLoginComponent {}
 
-@Component({ standalone: true, template: "Mock Teams Page" })
+@Component({
+  standalone: true,
+  template: "Mock Teams Page",
+  selector: "app-mock-teams",
+})
 class MockTeamsComponent {}
 
-@Component({ standalone: true, template: "Mock Transactions Page" })
+@Component({
+  standalone: true,
+  template: "Mock Transactions Page",
+  selector: "app-mock-transactions",
+})
 class MockTransactionsComponent {}
 
-@Component({ standalone: true, template: "Mock Profile Page" })
+@Component({
+  standalone: true,
+  template: "Mock Profile Page",
+  selector: "app-mock-profile",
+})
 class MockProfileComponent {}
 
-@Component({ standalone: true, template: "Mock About Page" })
+@Component({
+  standalone: true,
+  template: "Mock About Page",
+  selector: "app-mock-about",
+})
 class MockAboutComponent {}
 
-@Component({ standalone: true, template: "Mock Feedback Page" })
+@Component({
+  standalone: true,
+  template: "Mock Feedback Page",
+  selector: "app-mock-feedback",
+})
 class MockFeedbackComponent {}
 
 export const mockRoutes = [
